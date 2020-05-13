e375aa559333af396279072f9a12ac586d362123
bbackjk <wnsrud6839@gmail.com>
2020-05-13 11:48:32 +0900

    [Python] 200513 morning study

---
+++ python/200513/fahrenhite.py
@@ -0,0 +1,10 @@
+celsius = 37
+
+def fahrenhite(c):
+    return c * 9/5 + 32
+
+print("섭씨: ", celsius, ',', '화씨: ', fahrenhite(celsius))
+
+celsius += 3
+
+print("섭씨: ", celsius, ',', '화씨: ', fahrenhite(celsius))
---
+++ python/200513/sum.py
@@ -0,0 +1,11 @@
+def div(a):
+    print("Result:", a/2)
+
+def sum(a,b):
+    return a+b
+
+num = input("Input any number:")
+div(int(num))
+a = 1
+b = 2
+print(a,b, sum(a,b))
