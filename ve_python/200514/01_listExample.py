6e1cbc776ea00a665f6282d62bb253448f3ce594
bbackjk <wnsrud6839@gmail.com>
2020-05-14 11:45:49 +0900

    [VE_Python] 200514 morning study

---
+++ ve_python/200514/01_listExample.py
@@ -0,0 +1,8 @@
+# https://github.com/lee7py/Python-Programming/blob/master/Ch03/3-1-3.py
+
+list1 = [6, 4, -5, 3.5]
+list1.sort()
+print(list1)
+list2 = ["ha", "hi", "B", "7"]
+list2.sort()
+print(list2)
