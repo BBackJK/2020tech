132d57090334ccd740e342ad501a62ef219b6615
bbackjk <bbackjk1113@gmail.com>
2020-07-01 12:49:12 +0900

    [Django] 200701 Online Study 1

---
+++ django/PycharmProject/200701/HelloWorld/hello.py
@@ -0,0 +1,46 @@
+def display():
+    #TODO: 함수의 기능을 수행해야 함
+    print("hello python!!!")
+    print("hi!!!")
+
+
+
+
+
+
+
+
+'''
+sadsa
+ss
+a
+sas
+as
+da
+dasd
+sada
+d
+adas
+dsa
+das
+a
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+display()
